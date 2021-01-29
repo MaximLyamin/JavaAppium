b01f31100d3095c87c1c47b079141d54a9ee9b48
MaximLyamin <maxim.lyamin@kaspersky.com>
2021-01-29 04:38:58 +0300

    Update MWMyListsPageObject.java

--- src/test/java/lib/ui/mobile_web/MWMyListsPageObject.java
+++ src/test/java/lib/ui/mobile_web/MWMyListsPageObject.java
@@ -6,10 +6,8 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class MWMyListsPageObject extends MyListsPageObject {
 
     static {
-        //FOLDER_BY_NAME_XPATH_TPL = "xpath://*[@text='{FOLDER_NAME}']";
         ARTICLE_BY_TITLE_XPATH_TPL = "xpath://ul[contains(@class, 'watchlist')]//h3[contains(text(),'{TITLE}')]";
         REMOVE_FROM_SAVED_BUTTON = "xpath://ul[contains(@class, 'watchlist')]//h3[contains(text(),'{TITLE}')]/../../*[contains(@class, 'watched')]";
-        //"/html/body/div[1]/div/main/div[3]/ul/li/a[2]"
     }
 
     public MWMyListsPageObject(RemoteWebDriver driver) {
