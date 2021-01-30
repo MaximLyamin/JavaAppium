a58f8864c29b631e5e16f2bda63c0d72bda0b25e
MaximLyamin <maxim.lyamin@kaspersky.com>
2021-01-30 22:31:07 +0300

    Added tags for testPassThroughWelcome

--- src/test/java/tests/GetStartedTest.java
+++ src/test/java/tests/GetStartedTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import io.qameta.allure.*;
+import io.qameta.allure.junit4.DisplayName;
 import lib.CoreTestCase;
 import lib.Platform;
 import lib.ui.WelcomePageObject;
@@ -7,6 +9,11 @@ import org.junit.Test;
 
 public class GetStartedTest extends CoreTestCase {
     @Test
+    @Features(value = {@Feature(value = "FRW")})
+    @DisplayName("Pass First Run Wizard on iOS Device")
+    @Description("We open Welcome page and go to next page")
+    @Step("Starting test testPassThroughWelcome")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testPassThroughWelcome() {
 
         if ((Platform.getInstance().isAndroid()) || (Platform.getInstance().isMW())) {
